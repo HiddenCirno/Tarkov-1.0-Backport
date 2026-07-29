dbf523bf1c40c9d4240d0aaeda98c3c8ace50abc
GrooveypenguinX <grooveypenguinx@gmail.com>
2026-07-29 08:27:58 -0400

    version bump

--- WTT-ContentBackportPatcher/Patcher.cs
+++ WTT-ContentBackportPatcher/Patcher.cs
@@ -3,7 +3,7 @@
 namespace PunisherBossModPreloader
 {
 
-    [BepInPlugin("com.wtt.contentbackport", "Content Backport Preloader Patch", "1.1.1")]
+    [BepInPlugin("com.wtt.contentbackport", "Content Backport Preloader Patch", "1.1.2")]
     public class Patcher : BaseUnityPlugin
     {
 
